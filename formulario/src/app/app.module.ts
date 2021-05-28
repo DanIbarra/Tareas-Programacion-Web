291d395569dc1b2b4a947c7cf47cd406c8eb5862
Daniel <fcodany123@gmail.com>
2021-05-27 19:42:26 -0700

    Continuación

--- formulario/src/app/app.module.ts
+++ formulario/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ReactiveComponent } from './pages/reactive/reactive.component';
 import { TemplateComponent } from './pages/template/template.component';
@@ -16,7 +16,8 @@ import { TemplateComponent } from './pages/template/template.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- formulario/src/app/pages/reactive/reactive.component.ts
+++ formulario/src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-  constructor() { }
+  formulario: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.crearFormulario();
+    this.cargarDataFormulario();
+   }
 
   ngOnInit(): void {
+  }
+
+  crearFormulario(){
+    this.formulario = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(5)]],
+      apellido:['', Validators.required],
+      correo:['', Validators.required]
+    });
+  }
+
+  cargarDataFormulario(){
+    this.formulario.setValue({
+      nombre: 'Daniel',
+      apellido:'Ibarra',
+      correo: 'fcodany123@gmail.com'
+    })
+    
+
+  }
+  guardar(){
+    if (this.formulario.valid){
+      console.log(this.formulario.value);
+    }
+    else{
+      console.log('Su formulario no es valido');
+    }
   }
 
 }
