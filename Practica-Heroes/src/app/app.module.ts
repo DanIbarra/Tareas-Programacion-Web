285db5b7e4cbad2812bcb9b045d565728ace9db9
Daniel <fcodany123@gmail.com>
2021-06-01 18:36:48 -0700

    Creación de los componentes

--- Practica-Heroes/src/app/app.module.ts
+++ Practica-Heroes/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
