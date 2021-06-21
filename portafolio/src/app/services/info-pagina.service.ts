591931d0122bf734a4c4141596bfec393ac2cc91
Daniel <fcodany123@gmail.com>
2021-06-20 19:05:14 -0700

    Parte 5 completada

--- portafolio/src/app/services/info-pagina.service.ts
+++ portafolio/src/app/services/info-pagina.service.ts
@@ -10,11 +10,18 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
   constructor( private http: HttpClient) {
 
-    console.log('Servicio de infoPagina listo');
-
+    this.cargarInfo();
+    this.cargarEquipo();
     //Leer el archivo JSON
+
+    
+
+   };
+
+   private cargarInfo(){
 
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina )=> {
@@ -22,9 +29,18 @@ export class InfoPaginaService {
         this.cargada=true;
         this.info= resp;
 
+      });
+   }
+
+   private cargarEquipo(){
+    this.http.get('https://angular-html-93f3c-default-rtdb.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[] )=> {
+
+        
+        this.equipo= resp;
+
         console.log(resp);
 
       });
-
-   };
+   }
 }
